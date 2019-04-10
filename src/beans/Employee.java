a7c0ecd8db89fa5223aa62032f4bc5139c4f72c8
Mohideen Abdul Katheer M <abdulkatheerm@gmail.com>
2019-04-10 10:24:16 +0530

    Initial commit

---
+++ src/beans/Employee.java
@@ -0,0 +1,50 @@
+package beans;
+
+public class Employee {
+    private int empId;
+    private String name;
+    private String dept;
+    private int salary;
+
+    private Employee() {
+        System.out.println("Employee constructor called...");
+        System.out.println("Employee class is instantiating...");
+    }
+
+    public static Employee createEmployeeInstance(){
+        System.out.println("Employee::createEmployeeInstance() method called...");
+        return new Employee();
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
